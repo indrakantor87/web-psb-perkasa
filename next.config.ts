53b2c760dfc976d6f127a1163360e19975f2a34a
PSB Perkasa Developer <dev@psbperkasa.local>
2026-02-13 12:46:25 +0700

    fix: remove dangerous ignoreBuildErrors from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   poweredByHeader: false,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
