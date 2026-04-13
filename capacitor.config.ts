f3be68989c76f04a7b60080e10a500a0347aa25a
Perkasa Dev <dev@psbperkasa.local>
2026-04-14 01:23:17 +0700

    mobile: hardcode server url for in-app webview

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.psbperkasa.app',
   appName: 'PSB PERKASA',
-  webDir: 'mobile-build'
+  webDir: 'mobile-build',
+  server: {
+    androidScheme: 'https',
+    cleartext: false,
+    url: 'https://webpsb.perkasa.net.id'
+  },
+  allowNavigation: ['webpsb.perkasa.net.id', '*.perkasa.net.id']
 };
 
 export default config;
